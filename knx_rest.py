ac3eb12d2de9f5709399bdf79e76e5e40785abc3
lukas.bitter <lukas.bitter@gmail.com>
2017-11-22 22:22:09 +0100

    delete unnecessary code

--- knx_rest.py
+++ knx_rest.py
@@ -5,15 +5,9 @@ import logging
 
 app = Flask(__name__)
 
-
-@app.route('/', strict_slashes=False)
-def index():
-    return "Hello, World!"
-
-
 @app.route('/store/<int:floor_id>/<int:store_id>', methods=['POST'], strict_slashes=False)
 def setStore(floor_id, store_id):
-    # 200 2 2 3/4/1
+    # Example values: 200 2 2 3/4/1
     content = request.get_json()
     value = int(int(content['value']) * 255 / 100)
     size = '2'
@@ -32,82 +26,6 @@ def setRadiator(floor_id, radiator_id):
     group = '0/' + str(floor_id) + '/' + str(radiator_id)
     res = process(value, size, acpi, group)
     return res
-
-
-#######################################################################################################################
-############# BEACONS #################################################################################################
-#######################################################################################################################
-### THESE METHODS WERE SPECIALLY MADE FOR BEACONS ################################################################
-#######################################################################################################################
-
-"""
-@api {get} /beacon/<node_id>  get_beacon_devices
-@apiName get_beacon_devices
-@apiGroup Beacon
-
-
-@apiParam {Number} major_id Beacon's major ID
-
-
-@apiSuccess {String} Network_Home_ID Network's ID
-
-@apiSuccess {JSON} Node_<Number> A JSON containing node's information (for each node). See below
-
-@apiSuccess {boolean} Is_Ready Node status
-
-@apiSuccess {String[]} Neighbours Node's neighbours
-
-@apiSuccess {Number} Node_ID Node's ID
-
-@apiSuccess {String} Node_location Node's location
-
-@apiSuccess {String} Node_name Node's name
-
-@apiSuccess {String} Product_name Node's product name
-
-@apiSuccess {String} Query_stage Node object's readiness stage. Once it's at "Complete" stage, the Node object is ready to be used.
-
-@apiSuccess {Number} Query_stage_(%)  Node object's readiness stage (pourcentage). Once it's at 100%, the Node object is ready to be used.
-
-
-
-
-
-@apiSuccessExample {json} Example of result in case of success:
-{
-"Network Home ID": "0xe221b13f",
-"Node 1": {
-    "Is Ready": true,
-    "Neighbours": "2",
-    "Node ID": "1",
-    "Node location": "",
-    "Node name": "",
-    "Product name": "Z-Stick Gen5",
-    "Query Stage": "Complete",
-    "Query Stage (%)": "100 %"
-  },
-"Node 2": {
-    "Is Ready": true,
-    "Neighbours": "1",
-    "Node ID": "2",
-    "Node location": "",
-    "Node name": "",
-    "Product name": "MultiSensor 6",
-    "Query Stage": "Complete",
-    "Query Stage (%)": "100 %"
-  }
-}
-
-
-
-@apiDescription Gets information about the Z-Wave network in a JSON format
-"""
-
-'''
-@app.route('/beacon/<int:major_id>', strict_slashes=False)
-def get_beacon_devices(major_id):
-    return beacons.get_beacon_devices(major_id)
-'''
 
 if __name__ == '__main__':
     logging.basicConfig(level=logging.INFO)
